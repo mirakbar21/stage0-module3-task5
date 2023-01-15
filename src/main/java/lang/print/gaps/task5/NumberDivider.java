d3e52b5c953b983564d068ed6ce5d0b1258b3eab
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-01-15 22:31:24 +0600

    fix: dividers

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task5;
 
+import javax.crypto.spec.PSource;
+
 public class NumberDivider {
     void divide(int toBeDivided) {
+        System.out.println((float) toBeDivided / 5);
     }
 }
